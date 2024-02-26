2e3c5756f363c00784ae291dfca409a66a803efd
Aliaksandr Likhadziyeuski <alexanderlihodievski@gmail.com>
2024-02-26 16:33:46 +0100

    impl task

--- script.js
+++ script.js
@@ -1,1 +1,26 @@
-console.log('hello ')
+
+const numberOfFilms = prompt("Сколько фильмов вы уже посмотрели?", 0);
+
+
+const personalMovieDB = {
+
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    privat: false
+};
+
+
+const answer1 = prompt("Один из последних просмотренных фильмов?", "");
+const answer2 = prompt("На сколько оцените его?", "");
+
+const answer3 = prompt("Один из последних просмотренных фильмов?", "");
+const answer4 = prompt("На сколько оцените его?", "");
+
+personalMovieDB.movies[answer1] = answer2;
+personalMovieDB.movies[answer3] = answer4;
+
+
+console.log(personalMovieDB);
+
